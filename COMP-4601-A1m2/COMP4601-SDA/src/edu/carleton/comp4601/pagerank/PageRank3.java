84ee7d2cf39256b0778bd12f14eb3d65b6d7213f
julianClayton <julian.clayton@graphitesoftware.com>
2018-02-22 18:56:13 -0500

    Small change to pager ank

--- COMP-4601-A1m2/COMP4601-SDA/src/edu/carleton/comp4601/pagerank/PageRank3.java
+++ COMP-4601-A1m2/COMP4601-SDA/src/edu/carleton/comp4601/pagerank/PageRank3.java
@@ -125,11 +125,11 @@ public class PageRank3 {
 		ArrayList<Document> documents = DatabaseManager.getInstance().getAllDocuments();
 		ArrayList<HashMap<Integer, Float>> documentsWithRank = new ArrayList<HashMap<Integer, Float>>();
 		pageRankMatrix.print(pageRankMatrix.getRowDimension(), pageRankMatrix.getColumnDimension());
-		for (int i = 1; i < documents.size(); i++) {
+		for (int i = 0; i < pageRankMatrix.getColumnDimension(); i++) {
 			HashMap map = new HashMap<Integer, Float>();
-			map.put(documents.get(i).getId(), (float) pageRankMatrix.get(0, i-1));
-			boostMap.put(documents.get(i).getId(), (float) pageRankMatrix.get(0, i-1));
-			map.put(documents.get(i).getId(), (float) pageRankMatrix.get(0, i-1));
+			map.put(documents.get(i).getId(), (float) pageRankMatrix.get(0, i));
+			boostMap.put(documents.get(i).getId(), (float) pageRankMatrix.get(0, i));
+			map.put(documents.get(i).getId(), (float) pageRankMatrix.get(0, i));
 			documentsWithRank.add(map);
 		}
 		return documentsWithRank;
@@ -184,6 +184,8 @@ public class PageRank3 {
 		}
 		
 		Matrix matrix = new Matrix(doubleArray);
+		System.out.println("ADJACENCY MATRIX");
+		matrix.print(matrix.getRowDimension(), matrix.getColumnDimension());
 		return matrix;
 	}
 	
