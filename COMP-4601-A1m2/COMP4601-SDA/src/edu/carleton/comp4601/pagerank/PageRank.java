15a25cd23672de4116d9e5dd3b784be8dd9d3a3e
julianClayton <julian.clayton@graphitesoftware.com>
2018-02-23 17:01:12 -0500

    first submission commit

--- COMP-4601-A1m2/COMP4601-SDA/src/edu/carleton/comp4601/pagerank/PageRank.java
+++ COMP-4601-A1m2/COMP4601-SDA/src/edu/carleton/comp4601/pagerank/PageRank.java
@@ -1,8 +1,11 @@
 package edu.carleton.comp4601.pagerank;
-
 import Jama.Matrix;
+import edu.carleton.comp4601.SDA.db.DatabaseManager;
+import edu.carleton.comp4601.dao.Document;
+import edu.carleton.comp4601.graph.PageGraph;
+import edu.carleton.comp4601.graph.Vertex;
+import edu.carleton.comp4601.networking.Marshaller;
 
-import org.jgrapht.io.ExportException;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
@@ -10,19 +13,58 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
+
 import org.jgrapht.*;
+import org.jgrapht.generate.GraphGenerator;
 import org.jgrapht.graph.*;
 import org.jgrapht.io.*;
 
 public class PageRank {
-
+	final  double alpha =   0.5;
+	static Matrix pageRankMatrix;
+	private static PageRank instance;
+	private boolean rankComplete = false; 
+	private ArrayList<Vertex> vertexList;
+	private HashMap boostMap;
 	
-	final static double alpha =   0.5;
-	public static Matrix generateTransitionMatrix(Matrix A) {
+	PageGraph pg;
+	
+	public PageRank() {
+		instance= this;
+		try {
+			pg = (PageGraph) Marshaller.deserializeObject(DatabaseManager.getInstance().getGraphData());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		vertexList = new ArrayList<Vertex>();
+		boostMap = new HashMap<Integer, Float>();
+	}
+	
+	public static PageRank getInstance() {
+		if (instance == null) {
+			instance = new PageRank();
+		}
+		return instance;
+	}
+	
+	
+	public  int sumRow(Matrix A, int row) {
+	    int sum = 0;
+	    for(int i = 0; i < A.getColumnDimension(); i++) {
+	        sum += A.getArray()[row][i];
+	    }
+	    return sum;
+	}
+	
+	public  Matrix generateTransitionMatrix(Matrix A) {
 		Matrix B = A;
 		for (int row = 0; row < A.getRowDimension(); row++) {
 			double sum = sumRow(A, row);
-			System.out.println("Sum row " + sum);
 			for (int i = 0; i < A.getColumnDimension(); i++) {
 				if (sum == 0) {
 					B.getArray()[row][i] = 0;
@@ -32,63 +74,83 @@ public class PageRank {
 				}
 			}
 		}
-		System.out.println("transition matrix");
-		B.print(B.getRowDimension(), B.getColumnDimension());
 		return B;
 	}
-	
+	public  float getDocumentPageRank(int docId) {
 
-	public static Matrix multiplyMatrix(Matrix A, double a) {
-		A = A.timesEquals(a);
-		Matrix B = new Matrix(A.getRowDimension(), A.getColumnDimension(), a/A.getColumnDimension());
-		return A.plus(B);
+		float documentRank = (float) pageRankMatrix.get(0, docId);
+		return documentRank;
+}
+	private  Matrix multipleMatrixByAlpha(Matrix matrix) {
+
+		matrix = matrix.times((1.0 - alpha));
+		return matrix;
+
 	}
-	public static int sumRow(Matrix A, int row) {
-	    int sum = 0;
-	    for(int i = 0; i < A.getColumnDimension(); i++) {
-	        sum += A.getArray()[row][i];
-	    }
-	    return sum;
+
+	private  Matrix addMatrices(Matrix matrix) {
+		int row = matrix.getRowDimension();
+		int col = matrix.getColumnDimension();
+
+		Matrix additionMa = new Matrix(row, col, (alpha / (double) col));
+		Matrix result = matrix.plus(additionMa);
+
+		return result;
 	}
-	
-	private static Matrix getRow(Matrix A, int row) {
-		Matrix rowMatrix = new Matrix(1, A.getColumnDimension());
-		for (int i = 0; i < A.getColumnDimension(); i++) {
-			rowMatrix.getArray()[0][i] = A.getArray()[row][i];
-		}
-		return rowMatrix;
-	}
-	public static Matrix generateProbabilityMatrix(Matrix A) {
-		Matrix B = generateTransitionMatrix(A);
-		Matrix C = multiplyMatrix(B, 1-alpha);
-		return C;
-	}
-	private static Matrix convergeMatrix(Matrix P) {
+	public  ArrayList<HashMap<Integer, Float>> computePageRank() {
+		
+		Matrix adjacencyMatrix = generateAdjacencyMatrix(pg.getGraph());
+		Set<Vertex> vertexSet = pg.getGraph().vertexSet();
+		int size = adjacencyMatrix.getColumnDimension();
+		Matrix matrix = new Matrix(1, size);
+		matrix.set(0, 0, 1.0);
+
+		Matrix transitionMatrix = generateTransitionMatrix(adjacencyMatrix);
+		Matrix alphaMatrix = multipleMatrixByAlpha(transitionMatrix);
+		Matrix addMatrix = addMatrices(alphaMatrix);
 		double diff = 10000;
-		double threshold = 0.01;
-		Matrix p = P;
+		double threshold = 0.00000001;
+		
 		while (diff >= threshold) {
-			p = p.times(P);
-			diff = P.minus(p).normF();
-			P = p;
+			Matrix copy = matrix;
+			matrix = matrix.times(addMatrix);
+			matrix = matrix.times(1 / matrix.normInf());
+
+			diff = copy.minus(matrix).normF();
 		}
-		return P;
+		pageRankMatrix = matrix;
+		rankComplete = true;
+		
+		
+		ArrayList<Document> documents = DatabaseManager.getInstance().getAllDocuments();
+		ArrayList<HashMap<Integer, Float>> documentsWithRank = new ArrayList<HashMap<Integer, Float>>();
+		pageRankMatrix.print(pageRankMatrix.getRowDimension(), pageRankMatrix.getColumnDimension());
+		for (int i = 0; i < pageRankMatrix.getColumnDimension(); i++) {
+			HashMap map = new HashMap<Integer, Float>();
+			map.put(documents.get(i).getId(), (float) pageRankMatrix.get(0, i));
+			boostMap.put(documents.get(i).getId(), (float) pageRankMatrix.get(0, i));
+			map.put(documents.get(i).getId(), (float) pageRankMatrix.get(0, i));
+			documentsWithRank.add(map);
+		}
+		return documentsWithRank;
+
 	}
-	public static Matrix generatePageRankMatrix(Matrix A) {
-		Matrix P = generateProbabilityMatrix(A);
-		System.out.println("Probability Matrix");
-		P.print(P.getRowDimension(), P.getColumnDimension());
-		return convergeMatrix(P);
-	}
-	public static Matrix generateAdjacencyMatrix(Graph g) {
+	public  Matrix generateAdjacencyMatrix(Graph g) {
 		String fName = "adjacencymatrix";
 		BufferedWriter writer = null;
+		System.out.println(g.toString());
 		CSVExporter<String, DefaultEdge> csvExporter = new CSVExporter<String, DefaultEdge>(CSVFormat.MATRIX);
 		try {
 			writer = new BufferedWriter(new FileWriter(fName));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		
+		Set vertexSet = g.vertexSet();
+		vertexList = new ArrayList(vertexSet);
+		System.out.println("Size of vertex set: " + vertexList.size());
+		
 		csvExporter.exportGraph(g, writer);
 		
 		String thisLine; 
@@ -122,10 +184,17 @@ public class PageRank {
 		}
 		
 		Matrix matrix = new Matrix(doubleArray);
+		System.out.println("ADJACENCY MATRIX");
+		matrix.print(matrix.getRowDimension(), matrix.getColumnDimension());
 		return matrix;
 	}
 	
-	public static void main(String[] args) throws ExportException {
+	public HashMap<Integer, Float> generateBoostMap() {
+		
+		
+		return new HashMap<Integer, Float>();
+	}
+	public static void main(String[] args) {
 		Graph<String, DefaultEdge> g = new SimpleDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
 		double[][] array = {{1.,1.,0},{1.,1.,1.},{0.,1.,0.}};
 
@@ -145,10 +214,24 @@ public class PageRank {
         g.addEdge(v2, v1);
         g.addEdge(v2, v3);
 
-        Matrix matrix = generateAdjacencyMatrix(g);
-        matrix.print(matrix.getColumnDimension(), matrix.getRowDimension());
-        Matrix Pr = generatePageRankMatrix(matrix);
-        System.out.print("final");
-        Pr.print(Pr.getRowDimension(), Pr.getColumnDimension());
+        //matrix.print(matrix.getColumnDimension(), matrix.getRowDimension());
+        //Matrix Pr = computePageRank(g);
+        //Pr.print(Pr.getRowDimension(), Pr.getColumnDimension());
+       // Pr.print(Pr.getRowDimension(), Pr.getColumnDimension());
+        
+    	PageGraph graph = null;
+		try {
+			graph = (PageGraph) Marshaller.deserializeObject(DatabaseManager.getInstance().getGraphData());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println(graph.getGraph());
+        
+        getInstance().computePageRank();
 	}
+	public HashMap<Integer, Float> getBoostMap() {
+		return boostMap;
+	}
+
 }
