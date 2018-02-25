94dbb325fd424f2f2853eb9eb280b39b2da04f5d
lamcdougall <lauraamcdougall@gmail.com>
2018-02-25 18:18:11 -0500

    Prevent off-site visits and make crawl adaptive

--- COMP-4601-A1m2/COMP4601-SDA/src/edu/carleton/comp4601/crawler/Crawler.java
+++ COMP-4601-A1m2/COMP4601-SDA/src/edu/carleton/comp4601/crawler/Crawler.java
@@ -9,6 +9,7 @@ import edu.uci.ics.crawler4j.url.WebURL;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.ResourceBundle.Control;
 
 import Jama.Matrix;
 import edu.carleton.comp4601.SDA.db.DatabaseManager;
@@ -24,7 +25,7 @@ public class Crawler extends WebCrawler{
     	 
     	//prevent off-site visits
          String href = url.getURL().toLowerCase();
-         return  true;
+         return href.startsWith(Controller.SEED1) || href.startsWith(Controller.SEED2) || href.startsWith(Controller.SEED3);
      }
 
 
@@ -42,13 +43,13 @@ public class Crawler extends WebCrawler{
          
          int delay = this.getMyController().getConfig().getPolitenessDelay();
  	     
-         //long time = startTime.getTime() - endTime.getTime();
+         long time = startTime.getTime() - endTime.getTime();
          
-         /*System.out.println("Current delay: " + delay);
+         System.out.println("Current delay: " + delay);
          if (delay > time){
         	 System.out.println("Setting delay: " + time);
         	 this.getMyController().getConfig().setPolitenessDelay(delay*(int)time);
-         }*/
+         }
          
          
          Vertex v = new Vertex(docID, url);
